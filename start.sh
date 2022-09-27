34beb9ff345522ed986bff701e05a0e66a39b14d
“Muhammad <safriansah@gmail.com>
2022-09-27 14:26:07 +0700

    push fix docker volumes

--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+echo ${SCRIPTPATH}
+
 echo "### build images"
 docker build -t food-journal-backend:latest .
 echo "### remove container"
 docker stop food_journal
 docker rm food_journal
 echo "### start container"
-docker container create --name food_journal --restart=always -p 4000:4000 food-journal-backend:latest
+docker container create --name food_journal --restart=always -p 4000:4000 -v ${SCRIPTPATH}/public/images:/app/public/images food-journal-backend:latest
 docker cp .env food_journal:/app/.env
 docker start food_journal
