6922993e65b0302966fd3eae086f6d324a445013
Miftah Muhammad Farhan <miftahfarhan20@gmail.com>
2023-03-26 10:53:46 +0700

    initial commit

--- src/models/index.js
+++
@@ -1,9 +0,0 @@
-import db from "../config/Database.js";
-import "./FoodModel.js";
-import "./LikeModel.js";
-import "./UserModel.js";
-
-(async () => {
-    await db.sync({ alter: true });
-})();
-
--- src/routes/ImageRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { uploadImage } from '../controllers/ImageController.js';
-import { isAuth } from '../middleware/isAuth.js';
-
-const router = express.Router()
-
-router.post('/api/v1/upload-image', isAuth, uploadImage)
-
-export default router;
