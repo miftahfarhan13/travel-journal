7e911d6cb4f9a6fa31c233151f075bbd69e4c2d7
Miftah Muhammad Farhan <miftahfarhan20@gmail.com>
2023-07-03 12:09:51 +0700

    feat: new upload

--- index.js
+++ index.js
@@ -6,37 +6,21 @@ import PromoRoute from "./src/routes/PromoRoute.js"
 import ImageRoute from "./src/routes/ImageRoute.js"
 import CategoryRoute from "./src/routes/CategoryRoute.js"
 import ActivityRoute from "./src/routes/ActivityRoute.js"
-import multer from "multer";
 import bodyParser from "body-parser";
+import fileUpload from "express-fileupload";
 
 const app = express();
 app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 
-const fileStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'public/images')
-    },
-    filename: (req, file, cb) => {
-        cb(null, new Date().getTime() + '-' + file.originalname)
-    }
-})
-
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/png' ||
-        file.mimetype === 'image/jpg' ||
-        file.mimetype === 'image/jpeg' || 
-        file.mimetype === 'image/svg+xml' ||
-        file.mimetype === 'image/webp') {
-        cb(null, true)
-    } else {
-        cb(null, false)
-    }
-}
-
 app.use(bodyParser.json())
-app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'))
+
+app.use(
+    fileUpload({
+        abortOnLimit: true,
+    })
+);
 
 app.use(UserRoute)
 app.use(BannerRoute)
--- src/controllers/ImageController.js
+++ src/controllers/ImageController.js
@@ -5,19 +5,53 @@ const {
 } = process.env
 
 export const uploadImage = async (req, res) => {
+    const maxFileSize = 1024 * 1024
+
     try {
-        if (!req.file) {
-            const err = new Error('Image file is required')
-            err.errorStatus = 422;
-            throw err
+        const { image } = req.files;
+
+        // If no image submitted, exit
+        if (!image) {
+            return res.status(404).json({
+                code: "400",
+                status: "BAD_REQUEST",
+                message: "Image file is required"
+            })
         }
 
-        const image = req.file.filename
+        // If image size higher than 1 Mb
+        if (image.size > maxFileSize) {
+            return res.status(404).json({
+                code: "400",
+                status: "BAD_REQUEST",
+                message: "File size must be lower than 1 Mb"
+            })
+        }
+
+        // If does not have image mime type prevent from uploading
+        if (image.mimetype !== 'image/png' &&
+            image.mimetype !== 'image/jpg' &&
+            image.mimetype !== 'image/jpeg' &&
+            image.mimetype !== 'image/svg+xml' &&
+            image.mimetype !== 'image/webp') {
+
+            return res.status(404).json({
+                code: "400",
+                status: "BAD_REQUEST",
+                message: "File format must be PNG, JPG, JPEG, SVEG or WEBP"
+            })
+        }
+
+        const imageFileName = new Date().getTime() + '-' + image.name.replaceAll(' ', '-').toLowerCase()
+
+        // Move the uploaded image to our upload folder
+        image.mv('public/images/' + imageFileName);
+
         res.status(200).json({
             code: "200",
             status: "OK",
             message: "Upload image success",
-            url: `${BASE_URL}/images/${image}`
+            url: `${BASE_URL}/images/${imageFileName}`
         })
     } catch (error) {
         res.status(500).json({
